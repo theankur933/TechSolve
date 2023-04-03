630b03af8923a12cf9683625e83e9e15217f3778
Ankur Kharwar <theankurkharwar933@gmail.com>
2023-04-03 20:19:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function btn() {
+    var x = document.getElementById("middle");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
